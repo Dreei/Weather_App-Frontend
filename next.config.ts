7cf101db036a29be00b98e951ca4e2dd3845c997
Dreei <113105115+Dreei@users.noreply.github.com>
2025-01-29 14:26:40 +0800

    Refactor dir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Ensures App Router is enabled
+  },
 };
 
 export default nextConfig;
